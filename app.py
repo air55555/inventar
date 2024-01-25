1895a9f2477dc6b52ae6e90e6ff686babed712c5
1 <asa>
2024-01-25 22:56:28 +0300

    upd

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
-import subprocess
+from flask import Flask, render_template, request
+import subprocess, os
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
+#https://forums.developer.nvidia.com/t/help-needed-handling-images-in-python/107504
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/inv_num',methods =['POST'] )
+def inv_num_save():
+    if request.method=='POST':
+        r=request
+        nparr = np.fromstring(r.data, np.uint8)
+
+        im = Image.fromarray(nparr)
+        im.save("your_file.jpeg")
+        # decode image
+        #img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        #file = request.data
+        #files['file']
+        #img=file.read()
+        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'inv.jpg'))
+
+
 
 @app.route('/upload.php')
 def phpexample():
@@ -23,4 +43,4 @@ def run_php_script():
         return f"Error executing PHP script: {e.output}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0', port=80)
